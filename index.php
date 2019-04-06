fe8df6245fc29bd446fe9f4f2b19f8d55c973c67
Aakash Ahmed <aakashahmed18@gmail.com>
2019-04-06 19:56:17 +0500

    adding another file

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php echo "<H1>Hi, I am Usama</H1>"; ?>
+<?php
+	$name = "Usama";
+	$age = "21";
+	echo "My name is $name<br/>"."My age is $age";
+	
+?>
+<?php
+	function rank($n, $p)
+	{
+		echo "The name is $n";
+		echo "The position is $p";
+	} 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Usama_blog</title>
+</head>
+<body>
+</body>
+</html>
